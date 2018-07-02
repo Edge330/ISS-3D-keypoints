d0affc3c3eebfabf1404ca35ae4e3a12f3c91241
Edge330 <edge330@gmail.com>
2018-07-02 15:10:13 +0200

    .cpp source file

--- src/iss_3d_keypoints.cpp
+++ src/iss_3d_keypoints.cpp
@@ -5,7 +5,6 @@
 #include <pcl/features/normal_3d.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-
 int
 main(int, char** argv)
 {
@@ -19,13 +18,11 @@ if (pcl::io::loadPCDFile<pcl::PointXYZRGBA>(argv[1], *cloud) == -1) // load the 
 }
   std::cout << "points: " << cloud->points.size () <<std::endl;
 
- 
 pcl::ISSKeypoint3D<pcl::PointXYZRGBA, pcl::PointXYZRGB> iss_detector;
      
       pcl::PointCloud<pcl::PointXYZRGB>::Ptr keypoints (new pcl::PointCloud<pcl::PointXYZRGB> ());
       pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA> ());
           
-      
       iss_detector.setSearchMethod (tree);
       iss_detector.setSalientRadius (0.007f); 
       iss_detector.setNonMaxRadius (0.005f);  
@@ -36,14 +33,14 @@ pcl::ISSKeypoint3D<pcl::PointXYZRGBA, pcl::PointXYZRGB> iss_detector;
       iss_detector.setInputCloud (cloud); //
       iss_detector.compute (*keypoints); //
 
-
    std::cout << "N of ISS_3D points in the result are " << (*keypoints).points.size () << std::endl;
    pcl::io::savePCDFile("keypoints_iss_3d.pcd", *keypoints, true);
+   
+   // Visualization
 
    pcl::visualization::PCLVisualizer viewer("ISS 3D");
    viewer.addPointCloud(cloud, "scene");
    viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "scene");
-
 
    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> keypoints_color_handler(keypoints, 0, 0, 255);
    viewer.addPointCloud(keypoints, keypoints_color_handler, "scene_keypoints");
